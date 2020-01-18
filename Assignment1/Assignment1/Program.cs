1e3bcf80607a15c196ea6a6c247ef9eaf021a5be
DanJMaher <djm227@gmail.com>
2020-01-18 12:24:53 -0500

    Consolidated variable declarations.

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -4,46 +4,34 @@
     {
         static void Main(string[] args)
         {
-            //  Variable declarations
-            string firstName;
-            string middleInitial;
-            string lastName;
-            string fullName;
-            int age;
-            bool isCitizen;
-            bool canVote;
-            int heightFeet;
-            double heightInches;
-            double totalHeightCM;
-
             //  Retrieves name information from user
             System.Console.Write("Enter your first name: ");
-            firstName = System.Console.ReadLine();
+            string firstName = System.Console.ReadLine();
             System.Console.Write("Enter your middle initial: ");
-            middleInitial = System.Console.ReadLine();
+            string middleInitial = System.Console.ReadLine();
             System.Console.Write("Enter your last name: ");
-            lastName = System.Console.ReadLine();
+            string lastName = System.Console.ReadLine();
 
             //  Concatenates first, middle, and last names together
-            fullName = firstName + " " + middleInitial + ". " + lastName;
+            string fullName = firstName + " " + middleInitial + ". " + lastName;
 
             //  Retrieves height information from user
             System.Console.Write("Enter your height in feet: ");
-            heightFeet = int.Parse(System.Console.ReadLine());
+            int heightFeet = int.Parse(System.Console.ReadLine());
             System.Console.Write("Enter the inches beyond your base height: ");
-            heightInches = double.Parse(System.Console.ReadLine());
+            double heightInches = double.Parse(System.Console.ReadLine());
 
             //  Converts total height to CM
-            totalHeightCM = (heightFeet * 12 + heightInches) * 2.54;
+            double totalHeightCM = (heightFeet * 12 + heightInches) * 2.54;
 
             //  Retrieves age and citizen status information from user
             System.Console.Write("Enter your age: ");
-            age = int.Parse(System.Console.ReadLine());
+            int age = int.Parse(System.Console.ReadLine());
             System.Console.Write("Are you a citizen?  Enter true or false: ");
-            isCitizen = bool.Parse(System.Console.ReadLine());
+            bool isCitizen = bool.Parse(System.Console.ReadLine());
 
             //  Determines whether the user is eligible to vote
-            canVote = (age >= 18) && isCitizen;
+            bool canVote = (age >= 18) && isCitizen;
 
             //  Writes the user name, height in CM, and voter eligibility.
             System.Console.WriteLine();
