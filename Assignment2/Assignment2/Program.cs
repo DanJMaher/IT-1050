edc20532365034882d7a30a1bbce260c115c5ed4
DanJMaher <djm227@gmail.com>
2020-03-04 07:38:57 -0500

    Assignment 2 Complete

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -6,7 +6,28 @@ namespace Assignment2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person p1 = new Person();
+            Person p2 = new Person();
+
+            Console.WriteLine("\n[ Enter Information for Person 1 ]");
+            Person.GetPersonInfo(p1);
+
+            Console.WriteLine("\n[ Enter Information for Person 2 ]");
+            Person.GetPersonInfo(p2);
+
+            Console.WriteLine("[ Printing Results ]\n");
+            p1.PrintNameAndAge();
+            p1.spouse.PrintNameAndAge();
+            p2.PrintNameAndAge();
+            p2.spouse.PrintNameAndAge();
+
+
+            Person.sumOfAllAges = (p1.age + p2.age + p1.spouse.age + p2.spouse.age);
+            int personQuantity = 4;
+
+            Console.WriteLine("\nAverage Age: " + Person.sumOfAllAges / personQuantity);
+
+            Console.ReadKey();
         }
     }
 }
