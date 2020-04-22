a6a228131799f0f9a757f995c50840fd594e537f
DanJMaher <djm227@gmail.com>
2020-04-22 18:30:39 -0400

    Completed Assignment 5

---
+++ Assignment2/Assignment5/Assignment5/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment5
+{
+    class Person
+    {
+        public string _FirstName { get; private set; }
+        public string _LastName { get; private set; }
+
+        public Person(string firstName, string lastName)
+        {
+            _FirstName = firstName;
+            _LastName = lastName;
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine("\n{0} {1}", _FirstName, _LastName);
+        }
+    }
+}
---
+++ Assignment2/Assignment5/Assignment5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int peopleQuantity;
+            string input;
+
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("---------------Person Array Entry Tool---------------");
+            Console.WriteLine("-----------------------------------------------------\n");
+
+
+            while (true)
+            {
+                Console.Write("How many people do you wish to add to the array?: ");                
+                input = Console.ReadLine();
+
+                if (int.TryParse(input, out peopleQuantity) && peopleQuantity > 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("\n{0} is not a valid entry! Try again.\n", input );
+            }
+
+            Person[] people = new Person[peopleQuantity];
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                string firstName;
+                string lastName;
+
+                Console.Write("\nWhat is person {0}'s first name?: ", i + 1);
+                firstName = Console.ReadLine();
+
+                Console.Write("\nWhat is person {0}'s last name?: ", i + 1);
+                lastName = Console.ReadLine();
+
+                people[i] = new Person(firstName, lastName);
+            }
+
+            Console.WriteLine("\n---------------------------------------------");
+            Console.WriteLine("---------------People in array---------------");
+            Console.WriteLine("---------------------------------------------");
+
+            foreach (Person person in people)
+            {
+                person.PrintName();
+            }
+            Console.ReadKey();
+        }
+    }
+}
