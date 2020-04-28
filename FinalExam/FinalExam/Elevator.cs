025c8c2f8e4cfe16b1e421e17befa3195a54d2d1
DanJMaher <djm227@gmail.com>
2020-04-28 09:47:25 -0400

    Completed Final Exam

---
+++ FinalExam/FinalExam/Elevator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalExam
+{
+    class Elevator
+    {
+        private double MaxWeight;
+        private Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            Occupants = new Passenger[maxOccupants];
+
+            MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            double currentWeight = 0;
+            foreach (Passenger passenger in Occupants)
+            {
+                currentWeight += passenger.GetWeight();
+            }
+
+            return currentWeight;
+        }
+
+        public bool IsOverMaxCapacity()
+        {
+            if (GetCurrentWeight() > MaxWeight)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ FinalExam/FinalExam/Passenger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalExam
+{
+    class Passenger
+    {
+        private string Name;
+        private double Weight;
+
+        public Passenger(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public double GetWeight()
+        {
+            return Weight;
+        }
+    }
+}
